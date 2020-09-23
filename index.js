8d0bc951e76c0c7b1985915aea1206e42b100d2a
rounakmakwane <45486986+rounakmakwane@users.noreply.github.com>
2020-09-23 11:07:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda! test 1222'),
+    };
+    return response;
+};
