402bd5c60de84c39ec9736cadab148d11ef09cd1
rounakmakwane <45486986+rounakmakwane@users.noreply.github.com>
2020-09-23 12:43:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('expresss');
+var express = require('express');
 var bodyParser = require('body-parser');
 const URL = "https://wa.notifyabhi.com/api/whatsapp/messages";
 var axios = require('axios');
